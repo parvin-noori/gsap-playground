01488e3e2e9f7573b659cd12f6c7f66ec4e49c29
parvin-noori <parvinnoori0@gmail.com>
2025-02-10 15:37:04 +0330

    second

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,53 @@
 $(document).ready(function () {
-  let progressbar = $(".progress");
+  //-------------------------first-------------------------//
 
-  gsap.to(progressbar, {
-    width: "100%",
-    duration: 2,
-    ease: "power4.Out",
-  });
-  gsap.from("h1", {
-    y: -200,
-    ease: "elastic.out(1, 0.3)",
-    duration: 2,
-  });
-  gsap.to(progressbar, {
-    height: "100%",
-    top:0,
-    y:0,
-    delay:2
-  });
+  // let progressbar = $(".progress");
+
+  // gsap.to(progressbar, {
+  //   width: "100%",
+  //   duration: 2,
+  //   ease: "power4.Out",
+  // });
+  // gsap.from("h1", {
+  //   y: -200,
+  //   ease: "elastic.out(1, 0.3)",
+  //   duration: 2,
+  // });
+  // gsap.to(progressbar, {
+  //   height: "100%",
+  //   backgroundColor: "white",
+  //   top:0,
+  //   y:0,
+  //   delay:2
+  // });
+
+
+  //-------------------------second-------------------------//
+  
+  // const container = $(".container");
+  // const span = $("span");
+  
+  // let conatainerAnimate = gsap.to(container, {
+  //   width: "20vh",
+  //   height: "40vh",
+  //   borderRadius: "2%",
+  //   paused: true,
+  // });
+  // let textTween = gsap.to(span, {
+  //   x: 0,
+  //   fontSize: "2rem",
+  //   paused: true,
+  // });
+
+  // container.on("mouseenter", () => {
+  //   conatainerAnimate.play();
+  //   textTween.play();
+  // });
+  // container.on("mouseleave", () => {
+  //   conatainerAnimate.reverse();
+  //   textTween.reverse()
+  // });
+
+
+  //-------------------------third-------------------------//
 });
